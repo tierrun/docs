1b11588a9a98dc8887a61f66f071e290e053ce2a
Jerric Lyns John <jerriclynsjohn@outlook.com>
2023-06-24 19:19:56 +0530

    docs: adding recipes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -74,6 +74,7 @@ function useAutocomplete() {
 }
 
 function resolveResult(result) {
+  console.log(Object.keys(result.hierarchy))
   let allLevels = Object.keys(result.hierarchy)
   let hierarchy = Object.entries(result._highlightResult.hierarchy).filter(
     ([, { value }]) => Boolean(value)
@@ -289,41 +290,6 @@ function AlgoliaLogo(props) {
     </svg>
   )
 }
-
-// function SearchButton(props) {
-//   let [modifierKey, setModifierKey] = useState()
-
-//   useEffect(() => {
-//     setModifierKey(
-//       /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform) ? '⌘' : 'Ctrl '
-//     )
-//   }, [])
-
-//   return (
-//     <>
-//       <button
-//         type="button"
-//         className="hidden h-8 w-full items-center gap-2 rounded-full bg-white pl-2 pr-3 text-sm text-zinc-500 ring-1 ring-zinc-900/10 transition hover:ring-zinc-900/20 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:ring-white/20 lg:flex focus:[&:not(:focus-visible)]:outline-none"
-//         {...props}
-//       >
-//         <SearchIcon className="h-5 w-5 stroke-current" />
-//         Quick search...
-//         <kbd className="ml-auto text-2xs text-zinc-400 dark:text-zinc-500">
-//           <kbd className="font-sans">{modifierKey}</kbd>
-//           <kbd className="font-sans">K</kbd>
-//         </kbd>
-//       </button>
-//       <button
-//         type="button"
-//         className="flex h-6 w-6 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5 lg:hidden focus:[&:not(:focus-visible)]:outline-none"
-//         aria-label="Find something..."
-//         {...props}
-//       >
-//         <SearchIcon className="h-5 w-5 stroke-zinc-900 dark:stroke-white" />
-//       </button>
-//     </>
-//   )
-// }
 
 function SearchDialog({ open, setOpen, className }) {
   let router = useRouter()
