0f964264838495b171f487a43f17e8a949a0e537
Jerric Lyns John <jerriclynsjohn@outlook.com>
2023-06-27 11:51:02 +0530

    fix(seo): adding all old redirects and remapped to new paths

--- redirects.mjs
+++ redirects.mjs
@@ -48,6 +48,79 @@ export const redirects = [
   },
   // Redirects mapping old links
   {
+    source: '/docs/index',
+    destination: '/docs',
+    permanent: false,
+  },
+  {
+    source: '/docs/index/index.html',
+    destination: '/docs',
+    permanent: false,
+  },
+  {
+    source: '/docs/index.html',
+    destination: '/docs',
+    permanent: false,
+  },
+  {
+    source: '/docs/recipes/index.html',
+    destination: '/docs/recipes',
+    permanent: false,
+  },
+  {
+    source: '/docs/integrations',
+    destination:
+      '/docs/quickstarts/initiate-subscription#1-initialize-tier-sdk',
+    permanent: false,
+  },
+  {
+    source: '/docs/integrations/index.html',
+    destination:
+      '/docs/quickstarts/initiate-subscription#1-initialize-tier-sdk',
+    permanent: false,
+  },
+  {
+    source: '/docs/quickstart',
+    destination: '/docs#quickstarts',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli',
+    destination: '/docs/references/tier-cli',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/index.html',
+    destination: '/docs/references/tier-cli',
+    permanent: false,
+  },
+  {
+    source: '/docs/sdk',
+    destination: '/docs#explore-references',
+    permanent: false,
+  },
+  {
+    source: '/docs/sdk/index.html',
+    destination: '/docs#explore-references',
+    permanent: false,
+  },
+  {
+    source: '/docs/mapping',
+    destination: '/docs/miscellaneous/stripe-mapping',
+    permanent: false,
+  },
+  {
+    source: '/docs/mapping/index.html',
+    destination: '/docs/miscellaneous/stripe-mapping',
+    permanent: false,
+  },
+  {
+    source: '/blog/the-5-gotchas-of-stripes-test-mode',
+    destination: '/blog/the-5-gotchas-of-stripe-test-mode',
+    permanent: false,
+  },
+  // Pricing JSON
+  {
     source: '/pricing-json-schema',
     destination:
       'https://raw.githubusercontent.com/tierrun/tier/main/pricing/schema.json',
@@ -71,28 +144,28 @@ export const redirects = [
     permanent: false,
   },
   {
-    source: '/docs/quickstart',
-    destination: '/docs#quickstarts',
-    permanent: false,
-  },
-  {
-    source: '/docs/cli',
-    destination: '/docs/references/tier-cli',
-    permanent: false,
-  },
-  {
-    source: '/docs/sdk',
-    destination: '/docs#explore-references',
-    permanent: false,
-  },
-  {
-    source: '/blog/the-5-gotchas-of-stripes-test-mode',
-    destination: '/blog/the-5-gotchas-of-stripe-test-mode',
+    source: '/docs/pricing.json/index.html',
+    destination: '/docs/pricing-json',
     permanent: false,
   },
   // CLI Old Documentation remapping
   {
+    source: '/docs/install',
+    destination: '/docs/references/tier-cli#installing-tier-cli',
+    permanent: false,
+  },
+  {
+    source: '/docs/install/index.html',
+    destination: '/docs/references/tier-cli#installing-tier-cli',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/whois',
+    destination: '/docs/references/tier-cli#tier-whois',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/whois/index.html',
     destination: '/docs/references/tier-cli#tier-whois',
     permanent: false,
   },
@@ -102,7 +175,17 @@ export const redirects = [
     permanent: false,
   },
   {
+    source: '/docs/cli/whoami/index.html',
+    destination: '/docs/references/tier-cli#tier-whoami',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/phases',
+    destination: '/docs/references/tier-cli#tier-phases',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/phases/index.html',
     destination: '/docs/references/tier-cli#tier-phases',
     permanent: false,
   },
@@ -112,7 +195,17 @@ export const redirects = [
     permanent: false,
   },
   {
+    source: '/docs/cli/report/index.html',
+    destination: '/docs/references/tier-cli#tier-report',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/push',
+    destination: '/docs/references/tier-cli#tier-push',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/push/index.html',
     destination: '/docs/references/tier-cli#tier-push',
     permanent: false,
   },
@@ -122,7 +215,17 @@ export const redirects = [
     permanent: false,
   },
   {
+    source: '/docs/cli/clean/index.html',
+    destination: '/docs/references/tier-cli#tier-clean',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/version',
+    destination: '/docs/references/tier-cli#tier-version',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/version/index.html',
     destination: '/docs/references/tier-cli#tier-version',
     permanent: false,
   },
@@ -132,12 +235,27 @@ export const redirects = [
     permanent: false,
   },
   {
+    source: '/docs/cli/pull/index.html',
+    destination: '/docs/references/tier-cli#tier-pull',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/connect',
     destination: '/docs/references/tier-cli#tier-connect',
     permanent: false,
   },
   {
+    source: '/docs/cli/connect/index.html',
+    destination: '/docs/references/tier-cli#tier-connect',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/limits',
+    destination: '/docs/references/tier-cli#tier-limits',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/limits/index.html',
     destination: '/docs/references/tier-cli#tier-limits',
     permanent: false,
   },
@@ -152,7 +270,17 @@ export const redirects = [
     permanent: false,
   },
   {
+    source: '/docs/cli/subscribe/index.html',
+    destination: '/docs/references/tier-cli#tier-subscribe',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/serve',
+    destination: '/docs/references/tier-cli#tier-serve',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/serve/index.html',
     destination: '/docs/references/tier-cli#tier-serve',
     permanent: false,
   },
@@ -162,7 +290,17 @@ export const redirects = [
     permanent: false,
   },
   {
+    source: '/docs/cli/switch/index.html',
+    destination: '/docs/references/tier-cli#tier-switch',
+    permanent: false,
+  },
+  {
     source: '/docs/cli/ls',
+    destination: '/docs/references/tier-cli#tier-ls',
+    permanent: false,
+  },
+  {
+    source: '/docs/cli/ls/index.html',
     destination: '/docs/references/tier-cli#tier-ls',
     permanent: false,
   },
@@ -176,6 +314,13 @@ export const redirects = [
   {
     source: '/issues/new',
     destination: 'https://github.com/tierrun/tier/issues/new/choose',
+    permanent: false,
+  },
+  // Join Slack
+  {
+    source: '/slack',
+    destination:
+      'https://join.slack.com/t/tier-community/shared_invite/zt-1blotqjb9-wvkYMo8QkhaEWziprdjnIA',
     permanent: false,
   },
   {
